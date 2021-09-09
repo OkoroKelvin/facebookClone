68866f72a358fdb882c4a85b6678620a1bdcef1f
whalewalker <mongodbms@gmail.com>
2021-09-09 02:35:16 +0100

    update test folder

---
+++ src/test/UserTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
